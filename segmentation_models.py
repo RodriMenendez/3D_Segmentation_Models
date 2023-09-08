2f536c1a22470a917d567a2539831ef430234628
Angel Avelar <rodri@Angels-Air.lan>
2023-09-07 22:16:03 -0700

    added test images, masks, and evaluations

--- segmentation_models.py
+++ segmentation_models.py
@@ -11,6 +11,7 @@ class Model():
         self.image = None
         self.output = None
         self.prediction = None
+        self.mask = None
         self.model = None
         self.model_architecture = model_architecture.lower()
         self.spatial_dims = spatial_dims
@@ -28,7 +29,7 @@ class Model():
         if self.model_architecture == 'unet':
             self.model = models.U_Net(self.spatial_dims, self.in_channels, self.out_channels, self.channels, self.strides, self.kernel_size)
 
-        elif model_architecture == 'unetr':
+        elif self.model_architecture == 'unetr':
             self.model = models.UNETR(self.in_channels, self.out_channels, self.image_shape)
         else:
             raise Exception(f'{self.model_architecture} model not supported')
@@ -44,8 +45,8 @@ class Model():
         else:
             image = image.unsqueeze(0)
             return self.unsqueeze_image(image)
-
-    def set_image(self, image):
+    
+    def get_image(self, image):
         if isinstance(image, str):
             image = torch.tensor(imread(image))
         elif isinstance(image, torch.Tensor):
@@ -56,7 +57,13 @@ class Model():
             raise Exception('image is not str, torch.Tensor, or numpy.ndarray')
 
         image = self.unsqueeze_image(image)
-        self.image = image
+        return image
+
+    def set_image(self, image):
+        self.image = self.get_image(image)
+
+    def set_mask(self, mask):
+        self.mask = self.get_image(mask)
 
     def predict(self):
         with torch.no_grad():
@@ -67,20 +74,35 @@ class Model():
 
         self.prediction = evaluations.prediction(self.output)
 
-    def create_plots(self):
+    def create_plots(self, cmap='viridis'):
         if self.spatial_dims == 3:
             prediction_plot= np.argmax(self.prediction[0, 0], axis=0)
             image_plot = np.argmax(self.image[0, 0], axis=0)
+            mask_plot = np.argmax(self.mask[0, 0], axis=0)
         elif self.spatial_dims == 2:
             prediction_plot = self.prediction[0, 0]
             image_plot = self.image[0, 0]
+            mask_plot = self.mask[0, 0]
 
-        fig, axs = plt.subplots(1, 2, figsize=(10, 10))
-        axs[0].imshow(prediction_plot)
-        axs[0].set_title('Prediction Max Intensity' if self.spatial_dims==3 else 'Prediction')
-        axs[1].imshow(image_plot)
-        axs[1].set_title('Input Max Intensity' if self.spatial_dims==3 else 'Input')
+        fig, axs = plt.subplots(1, 3, figsize=(10, 10))
+        title = ' Max Intensity' if self.spatial_dims == 3 else ''
+        axs[0].imshow(prediction_plot, cmap=cmap)
+        axs[0].set_title('Prediction'+title)
+        axs[1].imshow(image_plot, cmap=cmap)
+        axs[1].set_title('Input'+title)
+        axs[2].imshow(mask_plot, cmap=cmap)
+        axs[2].set_title('Mask'+title)
         plt.show()
+
+    def get_evals(self):
+        confusion_matrix = evaluations.ConfusionMatrix(self.prediction, self.mask)
+        iou = evaluations.IoU(confusion_matrix)
+        precision = evaluations.precision(confusion_matrix)
+        accuracy = evaluations.accuracy(confusion_matrix)
+
+        evals = {'confusion matrix': confusion_matrix, 'iou': iou, 'precision': precision, 'accuracy': accuracy}
+
+        return evals
 
 
     def __str__(self):
